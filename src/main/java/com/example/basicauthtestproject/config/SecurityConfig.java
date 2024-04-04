ec2a1f17b677866cbc744e1d05d3f2ed3c26ca23
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-04-04 23:35:41 +0200

    login works

--- src/main/java/com/example/basicauthtestproject/config/SecurityConfig.java
+++ src/main/java/com/example/basicauthtestproject/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.basicauthtestproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,30 +27,63 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 
         // InMemoryUserDetailsManager
-        UserDetails admin = User.withUsername("Amiya")
-                .password(encoder.encode("123"))
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+//                .roles("ADMIN")
                 .roles("ADMIN", "USER")
                 .build();
 
-        UserDetails user = User.withUsername("Ejaz")
-                .password(encoder.encode("123"))
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("user"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
     }
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authConfig -> {
-                    authConfig.requestMatchers("/auth/welcome").permitAll();
+//                    authConfig.requestMatchers("/auth/welcome").permitAll();
                     authConfig.requestMatchers("/auth/user/**").authenticated();
                     authConfig.requestMatchers("/auth/admin/**").authenticated();
+                    authConfig.requestMatchers("/auth/site/**").authenticated();
                 })
                 .formLogin(withDefaults());
+//                .csrf(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests(auth2Config -> {
+                    auth2Config.requestMatchers("/auth/welcome").permitAll();
+                })
+                .csrf(Customizer.withDefaults());
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authConfig -> {
+//                    authConfig.requestMatchers("/auth/welcome").permitAll();
+//                    authConfig.requestMatchers("/auth/user/**").authenticated();
+//                    authConfig.requestMatchers("/auth/admin/**").authenticated();
+//                    authConfig.requestMatchers("/auth/login").authenticated();
+//                    authConfig.requestMatchers("/auth/site/**").permitAll();
+//                })
+//                .formLogin(loginConfig -> {
+//                    loginConfig.loginPage("/login");
+//                    loginConfig.permitAll();
+//                })
+//                .logout(logoutConfig -> {
+//                    logoutConfig.logoutSuccessUrl("/logout-success");
+//                    logoutConfig.permitAll();
+//                });
+//
+//        return http.build();
+//    }
 
     // Password Encoding
     @Bean
--- src/main/java/com/example/basicauthtestproject/controller/UserController.java
+++ src/main/java/com/example/basicauthtestproject/controller/UserController.java
@@ -1,28 +1,48 @@
 package com.example.basicauthtestproject.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class UserController {
 
+    @GetMapping("/site")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public String index(Authentication authentication, Model model) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+                model.addAttribute("isAdmin", true);
+            }
+            if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+                model.addAttribute("isUser", true);
+            }
+        }
+        return "index"; // This returns the logical view name of the Thymeleaf template
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome this endpoint is not secure";
+
+//        return "Welcome this endpoint is not secure";
+        return "index";
     }
 
     @GetMapping("/user/userProfile")
     @PreAuthorize("hasAuthority('ROLE_USER')")
     public String userProfile() {
+
         return "Welcome to User Profile";
     }
 
     @GetMapping("/admin/adminProfile")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public String adminProfile() {
+
         return "Welcome to Admin Profile";
     }
 
