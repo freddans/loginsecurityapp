d4f7c75beb5bf845796ceffa8d954a2ef1c7240c
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-04-04 17:40:35 +0200

    adding all

---
+++ src/main/java/com/example/basicauthtestproject/BasicauthtestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicauthtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicauthtestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicauthtestprojectApplication.class, args);
+	}
+
+}
